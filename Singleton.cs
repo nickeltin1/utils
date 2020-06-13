33fc52b09aecae5acad8fd840670238e2eea203d
nickeltin1 <59710264+nickeltin1@users.noreply.github.com>
2020-06-13 22:17:33 +0300

    Add files via upload

--- Singleton.cs
+++ Singleton.cs
@@ -4,16 +4,24 @@ namespace Utilities
 {
     public abstract class Singleton<T> : MonoBehaviour where T : Component
     {
+        /// <summary>
+        /// Generic singleton
+        /// Classes inhereted from this class will be singletones
+        /// To make sure that object exists is scene put it's prefab into Resources folder and name it after it's class name
+        /// </summary>
         private static T instance;
         public static T Instance 
         { 
             get
             {
+                //loads prefab form Resources if it's doesen's exist on scene
                 if (instance == null)
                     instance = (Instantiate(Resources.Load(typeof(T).Name) as GameObject)).GetComponent<T>();
                 return instance;
             } 
         }
+        
+        //Regural singleton pattern
         private void Awake()
         {
             if (instance == null) {
