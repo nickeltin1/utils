1757c35946f6720678697c38c4c8c73fc02c9ebc
nickeltin1 <59710264+nickeltin1@users.noreply.github.com>
2020-06-13 22:09:35 +0300

    Add files via upload

---
+++ Singleton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : Component
+    {
+        private static T instance;
+        public static T Instance 
+        { 
+            get
+            {
+                if (instance == null)
+                    instance = (Instantiate(Resources.Load(typeof(T).Name) as GameObject)).GetComponent<T>();
+                return instance;
+            } 
+        }
+        private void Awake()
+        {
+            if (instance == null) {
+                instance = this as T;
+                DontDestroyOnLoad (this);
+            } else {
+                Destroy (gameObject);
+            }
+        }
+    }
+}
