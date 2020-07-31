04c96a50918828edf85c64d826e526690258b7fe
Valentine <59710264+nickeltin1@users.noreply.github.com>
2020-07-31 20:46:34 +0300

    Add files via upload

---
+++ State.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Characters
+{
+    public class State
+    {
+        public Enum Type { get; private set; }
+        
+        public Action OnStateEnd { get; private set; }
+        public Action OnUpdate { get; private set; }
+        public Action OnFixedUpdate { get; private set; }
+        public Action OnStateStart { get; private set; }
+        
+        public State(Enum type, Action onStateEnd = null, Action onUpdate = null, Action onFixedUpdate = null, Action onStateStart = null)
+        {
+            Type = type;
+            OnStateEnd = onStateEnd;
+            OnUpdate = onUpdate;
+            OnFixedUpdate = onFixedUpdate;
+            OnStateStart = onStateStart;
+        }
+    }
+}
---
+++ StateMachine.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Characters
+{
+    public class StateMachine : MonoBehaviour
+    {
+        public enum StateType
+        {
+            Default
+        }
+        private State currentState;
+        private State mainState;
+        private Dictionary<Enum, State> states = new Dictionary<Enum, State>();
+
+        public static StateMachine Initialize(Transform parent, State[] states = null)
+        {
+            var instance = parent.gameObject.AddComponent<StateMachine>();
+            
+            if (states != null)
+            {
+                instance.currentState = states[0];
+                foreach (var state in states)
+                {
+                    instance.states.Add(state.Type, state);
+                }
+            }
+            return instance;
+        }
+
+        public State SwichtState(Enum type)
+        {
+            if (states.TryGetValue(type, out var requestedState))
+            {
+                if (requestedState == currentState) return null;
+                currentState.OnStateEnd?.Invoke();
+                currentState = requestedState;
+                currentState.OnStateStart?.Invoke();
+                return currentState;
+            }
+
+            Debug.LogWarning($"Requested state " + type + " doesn't exist");
+            return null;
+        }
+        public void AddState(State newState)
+        {
+            if (!states.ContainsKey(newState.Type))
+            {
+                states.Add(newState.Type, newState);
+                if (currentState == null) currentState = newState;
+            }
+        }
+        public void SetMainState(State defaultState)
+        {
+            this.mainState = defaultState;
+        }
+
+
+        private void Update()
+        {
+            mainState?.OnUpdate?.Invoke();
+            currentState?.OnUpdate?.Invoke();
+        }
+        private void FixedUpdate()
+        {
+            mainState?.OnFixedUpdate?.Invoke();
+            currentState?.OnFixedUpdate?.Invoke();
+        }
+    }
+}
